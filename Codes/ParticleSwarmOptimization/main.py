8100219ac5ccb378c26770e039a13fe4f97b50bf
chichuyun <442307054@qq.com>
2018-10-27 18:40:05 +0800

    Update main.py

--- Codes/ParticleSwarmOptimization/main.py
+++ Codes/ParticleSwarmOptimization/main.py
@@ -23,13 +23,15 @@ class PSO(object):
         self.p_best = None  # the best position of single particles
         self.g_best = None  # the best position of all particle
         self.values = None  # the values of particles
-        self.trace = list() # evolutionary track
+        self.trace = list() # the evolutionary track of the best particle
+        self.track = list() # the evolutionary track of all particles
 
     def initialize(self, lower, upper):
         """ initialization """
         self.lower = lower
         self.upper = upper
         self.pos = ran((self.num, self.dim))*(upper - lower) + lower
+        print(self.pos)
         self.vel = np.zeros_like(self.pos)
         self.p_best = self.pos.copy()
         self.values = np.array(list(map(self._fit, self.pos)))
@@ -47,6 +49,7 @@ class PSO(object):
     def evolve(self):
         """ iteration """
         for j in range(self.count):
+            self.track.append(list())
             for i in range(self.num):
                 self.vel[i] = self.w*self.vel[i] + \
                               self.p1*(self.p_best[i] - self.pos[i])*ran() + \
@@ -58,8 +61,8 @@ class PSO(object):
                                        self.upper, self.pos[i])
                 self.pos[i] = np.where(self.pos[i]<self.lower, \
                                        self.lower, self.pos[i])
+                self.track[-1].append(self.pos[i].copy())
             self.update_the_best_position()
-            print("%4d: %7.3f, %7.3f"%(j, self.trace[-1], self.g_best))
 
     def update_the_best_position(self):
         for i, best in enumerate(self.p_best):
@@ -83,13 +86,53 @@ class PSO(object):
 
 if __name__=="__main__":
 
-    def fun(x):
-        return x[0]+10*np.sin(5*x[0])+7*np.cos(4*x[0])
+    def fun(var):
+        x, y = var
+        return -(x*x + y*y)
 
-    process = PSO(1, 10, 20)  # dim, num, count
+    process = PSO(2, 10, 20)  # dim, num, count
     process.fitness = fun     # fitness function
-    process.initialize(0, 9)  # lower, upper
+    process.initialize(-5, 5) # lower, upper
 
     process.evolve()
     process.result() # 7.860, 24.853
-    process.plot()
+    process.plot()   # the evolution of values
+
+# ------------------------------------------------------------ # gif of evolution
+# to creat gif need install ffmpeg and moviepy
+    import moviepy.editor as mpy
+    from moviepy.video.io.bindings import mplfig_to_npimage
+    from mpl_toolkits.mplot3d import Axes3D
+
+    duration = 4    # s
+    fps = 5         # frame number: fps/s
+
+    plt.ioff()
+    def make_frame_mpl(i):
+        index = int(i*fps)
+        x = np.linspace(process.lower,process.upper,1000,endpoint=True)
+        X, Y = np.meshgrid(x, x)
+        Z = -fun([X, Y])
+
+        x, y, z = [], [], []
+        for xy in process.track[index]:
+            x.append(xy[0])
+            y.append(xy[1])
+            z.append(-fun(xy))
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.view_init(elev=48, azim=-61)
+        ax.plot_surface(X, Y, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+        ax.scatter(x, y, z, c='k', s=50, facecolor='k', alpha=1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('value')
+        ax.set_title(str(index)+'  value:  '+str(process.trace[index]))
+        ax.set_xlim(process.lower,process.upper)
+        ax.set_ylim(process.lower,process.upper)
+        return mplfig_to_npimage(fig)
+
+    animation = mpy.VideoClip(make_frame_mpl, duration=duration)
+    animation.write_gif('PSO.gif', fps=fps)
+    plt.ion()
