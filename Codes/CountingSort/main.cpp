2ddd0c5ec6ebca331d23212c8e406ff2f8c8998c
chichuyun <442307054@qq.com>
2020-04-06 20:38:50 +0800

    Update main.cpp
    add Counting Sort

---
+++ Codes/CountingSort/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef vector<int> vecInt;
+
+vecInt countingsort(vecInt &);
+
+int main() {
+    vecInt lst;
+    int in, num;
+    
+    cout<<"num: "<<endl;
+    cin>>num;
+    
+    cout<<"Input nums: "<<num<<endl;
+    for(int i=0;i<num;++i) {
+        cin>>in;
+        lst.push_back(in);
+    }
+    cout<<"Raw array: "<<endl;
+    for(int i=0;i<lst.size();++i) 
+        cout<<lst[i]<<" ";
+    cout<<endl;
+
+    vecInt res = countingsort(lst);
+    cout<<"Sored array: "<<endl;
+    for(int i=0;i<res.size();++i) 
+        cout<<res[i]<<" ";
+    cout<<endl;
+}
+
+vecInt countingsort(vecInt &lst) {
+    int num = 0;
+    for(int n : lst) num = max(num, n); // find the max number.
+    vector<int> counts(num+1,0);
+    for(int n : lst) ++counts[n];   // counting numbers
+    for(int i=1;i<=num;++i) counts[i] = counts[i] + counts[i-1];  // accumulation
+    vector<int> res(lst.size(),0);
+    for(int i=lst.size()-1;i>=0;--i) {
+        res[counts[lst[i]]] = lst[i];
+        --counts[lst[i]];
+    }
+    return res;
+}
