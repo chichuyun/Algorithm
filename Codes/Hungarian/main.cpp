4e3e116e263986e10006bb1c862e99b20175d285
chichuyun <442307054@qq.com>
2020-08-12 10:08:01 +0800

    Create main.cpp

---
+++ Codes/Hungarian/main.cpp
@@ -0,0 +1,56 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Hungarian {
+private:
+    vector<int> vis;
+    vector<bool> mems;
+    vector<vector<bool>> adjs;
+    int M, N;
+    int count;
+
+    bool match(int i) {
+        for(int j=0; j<N; ++j) {
+            if(adjs[i][j] && mems[j]) {
+                mems[j] = false;
+                if(vis[j] == -1 || match(vis[j])) {
+                    vis[j] = i;
+                    mems[j] = false;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    Hungarian(vector<vector<bool>> adjs) {
+        this->adjs = adjs;
+        M = adjs.size();
+        N = adjs[0].size();
+        count = 0;
+        mems.resize(N);
+        vis.resize(N, -1);
+    };
+
+    int solve() {
+        for(int i=0; i<M; ++i) {
+            for(int j=0; j<N; ++j) mems[j] = true;
+            if(match(i)) ++count;
+        }
+        return count;
+    }
+};
+
+int main() {
+    vector<vector<bool>> adjs = {{1, 0, 1},
+                                 {1, 1, 1},
+                                 {1, 0, 1}};
+
+    Hungarian *h = new Hungarian(adjs);
+    cout << h->solve() << endl;
+
+    return 0;
+}
